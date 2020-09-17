6d2f61c81f822a6fe5ca4f0946989ffbf4133104
real-myth <mithilsmail@gmail.com>
2020-09-16 20:22:11 -0700

    modified:   about_us.html 	modified:   assets/css/main.css 	modified:   assets/js/main.js 	modified:   index.html 	new file:   package-lock.json 	deleted:    sign_up.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,25 @@
 	templated.co @templatedco
 	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
 */
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
 
 (function($) {
 
